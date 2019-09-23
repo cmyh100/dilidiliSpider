4c0f2f7327e65421e023d10f9857e2d9b22c409a
cmyh100 <455392633@qq.com>
2019-09-23 23:46:08 +0800

    v1.0

--- src/diliSpider/index.js
+++ src/diliSpider/index.js
@@ -1,5 +1,6 @@
 const queryselector = require('./queryselector.js');
 const util = require('../util/index.js');
+const Excel = require('exceljs');
 
 class DiliSpider {
   constructor (options) {
@@ -8,9 +9,9 @@ class DiliSpider {
   }
   
   /**
-   * @description 初始化
+   * @description 初始化数据
    */
-  init (options) {
+  init () {
     const startobj = util.formatTime(this.options.startTime);
     const endobj = util.formatTime(this.options.endTime);
     this.url = this.options.url || '';
@@ -18,29 +19,25 @@ class DiliSpider {
     this.currentM = startobj.m || '';
     this.endY = endobj.y || '';
     this.endM = endobj.m || '';
+    this.data = [];
     this.runMain();
   }
   
+  /**
+   * @description 主任务函数
+   */
   async runMain () {
     const data = await queryselector.getAnimeList(this.url, this.currentY, this.currentM);
-    console.log(data);
+    this.data = [
+      ...this.data,
+      ...data
+    ];
     this.checkOver();
   }
   
-  runNext () {
-    if (this.currentM * 1 === 12) {
-    	this.currentM = '01';
-    	this.currentY = this.currentY * 1 + 1;
-    } else {
-    	this.currentM * 1 >= 9 ? (this.currentM = this.currentM * 1 + 1 + '') : (this.currentM = '0' + (this.currentM * 1 + 1));
-    }
-    this.runMain();
-  }
-  
-  runEnd () {
-    console.log('end');
-  }
-  
+  /**
+   * @description 检查任务是否结束
+   */
   checkOver () {
     if (this.currentY === this.endY && this.currentM === this.endM) {
       this.runEnd();
@@ -51,13 +48,59 @@ class DiliSpider {
     }
   }
   
-  getImage () {
-    
+  /**
+   * @description 进入下一个周期
+   */
+  runNext () {
+    if (this.currentM * 1 === 12) {
+    	this.currentM = '01';
+    	this.currentY = this.currentY * 1 + 1;
+    } else {
+    	this.currentM * 1 >= 9 ? (this.currentM = this.currentM * 1 + 1 + '') : (this.currentM = '0' + (this.currentM * 1 + 1));
+    }
+    this.runMain();
   }
-}
-
-const checkOptions = () => {
   
+  /**
+   * @description 收尾函数
+   */
+  runEnd () {
+    // console.log(this.data);
+    let columns = queryselector.getColumns() || [];
+    this.createExcel(`./excel/anime_info${this.options.startTime}_${this.options.endTime}.xlsx`, columns, this.data)
+  }
+
+  /**
+   * @description 创建Excel
+   */
+  createExcel(_filename, _columns, _dataSource) {
+    // let start_time = new Date();
+    let workbook = new Excel.stream.xlsx.WorkbookWriter({
+      filename: _filename
+    });
+    let worksheet = workbook.addWorksheet('Sheet');
+    worksheet.columns = _columns
+    let data = _dataSource
+    let length = data.length;
+    let current_num = 0;
+    let time_monit = 400;
+    let temp_time = Date.now();
+    console.log(_filename + ' 开始添加数据');
+    // 开始添加数据
+    for (let i in data) {
+      worksheet.addRow(data[i]).commit();
+      current_num = i;
+      if (Date.now() - temp_time > time_monit) {
+        temp_time = Date.now();
+        console.log((current_num / length * 100).toFixed(2) + '%');
+      }
+    }
+    console.log('添加数据完毕：', length);
+    workbook.commit();
+    // let end_time = new Date();
+    // let duration = end_time - start_time;
+    // console.log('用时：' + duration);
+  }
 }
 
 module.exports = DiliSpider;
--- src/diliSpider/queryselector.js
+++ src/diliSpider/queryselector.js
@@ -1,31 +1,87 @@
 const http = require('http');
 const cheerio = require('cheerio');
 
-const getAnimeList = (url, y, m) => {
+const httpProxy = (_url) => {
   return new Promise((resolve, reject) => {
-    const _url = `${url}${y}${m}/`;
     console.log(`Start Get Anime: ${_url}`);
-    let htmlstr = '';
+    let htmlsrteam = '';
     http.get(_url, function (res) {
       if (res.statusCode === 200) {
         res.on('data', function (chunk) {
-          console.log(chunk);
-          htmlstr += chunk;
+          htmlsrteam += chunk;
         });
         res.on('end', async function () {
-          console.log('end');
-          const $ = cheerio.load(html);
-          const domlist = $('.anime_list dl');
-          resolve(true);
+          resolve(cheerio.load(htmlsrteam));
         });
         res.on('error', function (e) {
           console.log('\x1B[31m%s\x1b[0m:', `Got Error-Msg: ${e}`);
         });
+      } else {
+        resolve('');
       }
     });
   });
 }
 
+/**
+ * @description 此处自定义Dom相关操作
+ */
+const getAnimeList = async (url, y, m) => {
+  const _url = `${url}${y}${m}/`;
+  const $ = await httpProxy(_url);
+  if (!$) {return []}
+  const domlist = $('.anime_list dl');
+  let datalist = [];
+  for (let i = 0; i < domlist.length; i++) {
+    const imgurl = domlist.eq(i).find('dt img').attr('src');
+    const name = domlist.eq(i).find('dd h3 a').text();
+    const country = domlist.eq(i).find('dd .d_label').eq(0).find('a').text();
+    const time = domlist.eq(i).find('dd .d_label').eq(1).find('a').text();
+    const label = domlist.eq(i).find('dd .d_label').eq(2).find('a').text();
+    const company = domlist.eq(i).find('dd .d_label').eq(3).find('a').text();
+    const keypoint = domlist.eq(i).find('dd>p').eq(4).text();
+    const singer = domlist.eq(i).find('dd>p').eq(7).text();
+    const outline = domlist.eq(i).find('dd>p').eq(8).text();
+    const state = domlist.eq(i).find('dd>p').eq(9).text();
+    datalist.push({
+      imgurl,
+      name,
+      country,
+      time,
+      label,
+      company,
+      keypoint,
+      singer,
+      outline,
+      state,
+    });
+  }
+  return datalist;
+}
+
+const getColumns = () => {
+  let columns = [];
+  [
+    'imgurl',
+    'name',
+    'country',
+    'time',
+    'label',
+    'company',
+    'keypoint',
+    'singer',
+    'outline',
+    'state'
+  ].map(item => {
+    columns.push({
+      header: item,
+      key: item
+    });
+  });
+  return columns
+}
+
 module.exports = {
-  getAnimeList
+  getAnimeList,
+  getColumns
 };
